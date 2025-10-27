f2352df70dee7f3024cacb65916e40c9eabb60cc
swastikPY <swastikvishwa908@gmail.com>
2025-10-27 18:08:14 +0530

    fixed a bug of props i made

--- 06_currencyconverter/src/App.jsx
+++ 06_currencyconverter/src/App.jsx
@@ -64,7 +64,7 @@ function App() {
                                 amount = {convertedAmount}
                                 currencyOption = {options}
                                 onCurrencyChange = {(currency) => setTo(currency)}
-                                selectCurrency = {from}
+                                selectedCurrency = {to}
 
                             />
                         </div>
