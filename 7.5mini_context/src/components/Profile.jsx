16e57b100e8f49ac86ec6ff49e11545ed8261adb
swastikPY <swastikvishwa908@gmail.com>
2025-10-31 11:02:36 +0530

    seen context api  working

---
+++ 7.5mini_context/src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React, {useContext} from 'react'
+import UserContext from '../context/UserContext'
+
+function Profile() {
+  const { user } = useContext(UserContext)
+    
+    if (!user)  return <h2>Please login to view profile</h2>
+
+    return (
+    <div>
+        <h2>Profile</h2>
+        <p>Username: {user.username}</p> 
+    </div>
+  )
+}
+
+export default Profile
